d6fcdd0ea0c552f1fd8f050ba2a5fea764c0d00b
luiqor <vohnenna@gmail.com>
2024-04-17 14:05:07 +0300

    tasks from exercises 002 done

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,24 +1,31 @@
 export function getFillings(sandwich) {
-	if (sandwich === undefined) throw new Error('ingredients is required');
-	// Your code here!
+	if (sandwich === undefined) throw new Error("ingredients is required");
+	return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
-	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+	if (person === undefined) throw new Error("person is required");
+	return person.city === "Manchester";
 }
 
 export function getBusNumbers(people) {
-	if (people === undefined) throw new Error('people is required');
-	// Your code here!
+	if (people === undefined) throw new Error("people is required");
+	return Math.ceil(people / 40);
 }
 
 export function countSheep(arr) {
-	if (arr === undefined) throw new Error('arr is required');
-	// Your code here!
+	if (arr === undefined) throw new Error("arr is required");
+	return arr.reduce(
+		(accumulator, currentValue) =>
+			currentValue === "sheep" ? ++accumulator : accumulator,
+		0
+	);
 }
 
 export function hasMPostCode(person) {
-	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+	if (person === undefined) throw new Error("person is required");
+	return (
+		person.address.postCode.startsWith("M") &&
+		person.address.city === "Manchester"
+	);
 }
